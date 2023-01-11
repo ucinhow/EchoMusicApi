6ad895f4637126325921c0334174761cc675c825
ucinhow <ucinhow@gmail.com>
2023-01-12 04:27:00 +0800

    update: adjust error handle logic

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import Koa from "koa";
+import Koa, { Context } from "koa";
 import { koaBody as bodyparser } from "koa-body";
 import logger from "koa-logger";
 import router from "@/controller/route";
-import { AxiosError } from "axios";
+import { isAxiosError } from "axios";
 import { DEVELOPMENT_ENV, ERROR_MSG } from "./common/constant";
 import config from "./common/config";
 
@@ -11,30 +11,43 @@ server.use(bodyparser());
 server.use(logger());
 server.use(router.allowedMethods()).use(router.routes());
 
-server.on("error", (err, ctx) => {
-  if (err instanceof AxiosError) {
+const errorHandler = (err: unknown, ctx: Context) => {
+  if (isAxiosError(err)) {
+    config.env === DEVELOPMENT_ENV &&
+      console.log(
+        `ServerError: request data error(${err.message})`,
+        `${err.response?.config.baseURL}/${err.response?.config.url}`
+      );
     ctx.status = 500;
     ctx.message = "ServerError: request data error";
-    config.env === DEVELOPMENT_ENV &&
-      console.log(`ServerError: request data error(${err.message})`);
-  } else if (err instanceof Error) {
-    if (err.message === ERROR_MSG.ParamError) {
-      ctx.status = 400;
-      ctx.message = "ClientError: invalid params";
-    } else if (err.message === ERROR_MSG.KuwoTokenError) {
-      ctx.status = 500;
-      ctx.message = "ServerError: request data error";
-      config.env === DEVELOPMENT_ENV &&
-        console.log("ServerError: kuwo csrf token error");
-    } else {
-      ctx.status = 500;
-      config.env === DEVELOPMENT_ENV &&
-        console.log(`ServerError: ${err.stack}`);
-    }
-  } else {
-    ctx.status = 500;
-    config.env === DEVELOPMENT_ENV && console.log(`ServerError: ${err}`);
+    return;
   }
-});
+  if (err instanceof Error) {
+    switch (err.message) {
+      case ERROR_MSG.ParamError: {
+        ctx.status = 400;
+        ctx.message = "ClientError: invalid params";
+        return;
+      }
+      case ERROR_MSG.KuwoTokenError: {
+        config.env === DEVELOPMENT_ENV &&
+          console.log("ServerError: kuwo csrf token error");
+        ctx.status = 500;
+        ctx.message = "ServerError: request data error";
+        return;
+      }
+      case ERROR_MSG.BannerJumpError: {
+        config.env === DEVELOPMENT_ENV &&
+          console.log(`ServerError: banner jump calc error`, err.stack);
+        ctx.status = 500;
+        return;
+      }
+    }
+  }
+  config.env === DEVELOPMENT_ENV && console.log(err);
+  ctx.status = 500;
+};
+
+server.on("error", errorHandler);
 
 export default server;
